85074348f7518c904edaac4442098e8a1f889a3e
opussf <github@zz9-za.com>
2019-03-24 04:44:51 -0700

    Change shot setup to be from a list.  shotDesc is now a function, missed shot now uses function to desc shot.

--- basketball_shot_log/logGen.py
+++ basketball_shot_log/logGen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import random
-random.seed( 15364 )
+random.seed( 76576 )
 
 players = [
 		( "t1 p1", "t1 p2", "t1 p3", "t1 p4", "t1 p5", "t1 p6", "t1 p7" ),
@@ -16,7 +16,7 @@ quarterTime = 12 * 60
 # 3 = shot - made - 3 pt
 # 4 = foul - penality shot
 activityList = [0] * 90
-for t in range( 1, 5 ):
+for t in [ 1, 1, 2, 3, 4 ]:
 	while True:
 		index = random.randrange( 0, len( activityList ) )
 		if activityList[index] == 0:
@@ -38,6 +38,13 @@ shotDescritptions = [ [], [],
 		[],  # 4 foul shot
 ]
 
+def shotDesc( shotType ):
+	# return a string, describing the shot
+	# shotType = 2 | 3 for number of points tried
+	shotRange = { 2: ( 5, 22 ), 3: ( 22, 47 ) }
+	distanceInfo = { "distanceStr": "%i-foot" % ( random.randint( *shotRange[shotType] ), ) }
+	return shotDescritptions[shotType][random.randrange( 0, len( shotDescritptions[shotType] ) )] % distanceInfo
+
 
 score = [0,0]
 for t in xrange( quarterTime, 0, -1 ):
@@ -51,18 +58,16 @@ for t in xrange( quarterTime, 0, -1 ):
 			shotInfo = [ madeMissed( shotsMade ), "penality shots %i of 2" % ( shotsMade, ) ]
 			score[team] += shotsMade
 		elif activity == 3:
-			distanceInfo = { "distanceStr": "%i-foot" % ( random.randint( 22, 47 ) ) }
 			shotInfo = [ madeMissed( 3 ),
-					shotDescritptions[activity][random.randrange( 0, len( shotDescritptions[activity] ) )] % distanceInfo,
-					"3-point shot" ]
+					shotDesc( activity ),
+					"3-pointer" ]
 			score[team] += 3
 		elif activity == 2:  #
-			distanceInfo = { "distanceStr": "%i-foot" % ( random.randint( 5, 22 ) ) }
 			shotInfo = [ madeMissed( 2 ),
-					shotDescritptions[activity][random.randrange( 0, len( shotDescritptions[activity] ) )] % distanceInfo ]
+					shotDesc( activity ) ]
 			score[team] += 2
 		elif activity == 1:  # shot missed
-			shotInfo = [ madeMissed( 0 ), "distance type" ]
+			shotInfo = [ madeMissed( 0 ), shotDesc( random.randint( 2, 3 ) ) ]
 
 		print "%s\t\t%s %s %s" % ( sec2Time( t ), player,
 				" ".join( shotInfo ), "%i - %i" % ( score[0], score[1] ) )
