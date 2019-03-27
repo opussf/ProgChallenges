3ae7316ae2c200f15d27d23f004b8cbe321e1df7
opussf <github@zz9-za.com>
2019-03-26 19:50:17 -0700

    2 types of function

--- number_persistance/persistance.py
+++ number_persistance/persistance.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-def per( n, steps=0, nums=[] ):
+def per_recursive( n, steps=0, nums=[] ):
 	if steps == 0:
 		nums = []
 	nums.append( n )
@@ -14,9 +14,21 @@ def per( n, steps=0, nums=[] ):
 	for j in digits:
 		result *= j
 
-	steps, nums = per( result, steps+1, nums )
+	steps, nums = per_recursive( result, steps+1, nums )
 	return steps, nums
 
+def per_iterative( n ):
+	steps = 0
+	nums = []
+	while len( str( n ) ) > 1:
+		steps += 1
+		nums.append( n )
+		result = 1
+		for j in [ int( i ) for i in str( n ) ]:
+			result *= j
+		n = result
+	return steps, nums
 
 if __name__=="__main__":
-	assert( per( 277777788888899 )[0] == 11 )
+	assert( per_recursive( 277777788888899 )[0] == 11 )
+	assert( per_iterative( 277777788888899 )[0] == 11 )
