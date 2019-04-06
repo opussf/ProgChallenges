3d60def346f1e818b101ea80b4947e49b5ba5c47
opussf <github@zz9-za.com>
2019-04-06 03:18:44 -0700

    Add options for time, max value, and interval.  Also adding saving steps to a json file.

--- number_persistence/opussf/findem.py
+++ number_persistence/opussf/findem.py
@@ -1,16 +1,42 @@
 #!/usr/bin/env python
 
-import sys, os
-
+import sys, os, time
 sys.path.insert( 1, os.path.join( sys.path[0], '..' ) )
-
 from persistence import *
 per = per_iterative
 
-lowestMax = {}
-allNumsBySteps = {}
-for vln in xrange( sys.maxint ):
-#for vln in xrange( 10000 ):
+from optparse import OptionParser
+import json
+
+parser = OptionParser()
+parser.add_option( "", "--runtime", action="store", dest="runtime", type="string", default="3600",
+		help="how long to run for. 1h, 60m, 3600s, 3600 are all the same." )
+parser.add_option( "-i", "--interval", action="store", dest="interval", type="int", default=500000,
+		help="how often to report progress." )
+parser.add_option( "-m", "--maxValue", action="store", dest="maxValue", type="int", default=sys.maxint-1,
+		help="max value to go to." )
+
+(options, args) = parser.parse_args()
+jsonFile = "persistence.json"
+
+try:
+	storedData = json.load( open( jsonFile, "r" ), parse_int=int )
+except:
+	storedData = { "startVal": 0, "lowestMax": {}, "allNumsBySteps": {} }
+
+print "Starting at:", storedData["startVal"]
+
+endTime = time.time() + int( options.runtime )   # since this should have unit in it, it needs to be decoded
+
+def storeValues( vln ):
+	storedData["startVal"] = vln
+	storedData["lowestMax"] = lowestMax
+	storedData["allNumsBySteps"] = allNumsBySteps
+	json.dump( storedData, open( jsonFile, "w" ) )
+
+lowestMax = dict( [ (int(k), v) for k, v in storedData["lowestMax"].items() ] )
+allNumsBySteps = dict( [ (int(k), v) for k, v in storedData["allNumsBySteps"].items() ] )
+for vln in xrange( storedData["startVal"] + 1, options.maxValue+1 ):
 	steps, nums = per( vln )
 	hasSteps = allNumsBySteps.get( steps, None )
 	if not hasSteps:
@@ -19,10 +45,13 @@ for vln in xrange( sys.maxint ):
 	if steps not in lowestMax.keys():
 		lowestMax[steps] = vln
 		print lowestMax
-	if vln % 500000 == 0:
-		print "Counts: ", allNumsBySteps
-
-allKeys = allNumsBySteps.keys()
-allKeys.sort()
-for k in allKeys:
-	print "%3i : %3i" % ( k, len( allNumsBySteps[k] ) )
+	if vln % options.interval == 0:
+		print "%s (%0.2f): %s" % ( vln, vln / sys.maxint * 100, allNumsBySteps )
+		storeValues( vln )
+	if time.time() >= endTime:
+		break
+try: vln
+except: pass
+else:
+	storeValues( vln )
+print "Ending:", lowestMax
