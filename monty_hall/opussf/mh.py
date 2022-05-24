64b8f1b4351c29a49bb89793b724fd6d688f405f
opussf <github@zz9-za.com>
2022-05-24 00:02:37 -0700

    Stuff

--- monty_hall/opussf/mh.py
+++ monty_hall/opussf/mh.py
@@ -1,5 +1,61 @@
 #!/usr/bin/env python
 
-def MontyHall( ):
-	pass
+import random
 
+class MontyHall( object ):
+	prize = 1
+	choosen = 2
+	opened = 4
+	removed = 8
+
+	def __init__( self, switch=False ):
+		random.seed()
+		self.actions = []
+		self.switch = switch
+
+	def __setup( self ):
+		self.doors = [0,0,0]  # bit fields, 1 = prize, 2 = choosen, 4 = opened, 8 = removed
+		door = random.randint(0,2)
+		self.doors[door] = 1
+		self.actions.append(door+1)
+		print( self.doors )
+
+	def __chooseDoor( self ):
+		canChoose = map( lambda x: False if x & self.opened \
+										 or x & self.removed \
+										 or (self.switch and not ( x & self.choosen )) \
+										 else True, self.doors )
+		guessing = True
+		while guessing:
+			door = random.randint(0,2)
+			if canChoose[door]:
+				self.doors[door] = self.doors[door] | self.choosen
+				guessing = False
+		self.actions.append(door+1)
+		print( canChoose )
+		print( self.doors )
+
+
+
+	def __revealDoor( self ):
+		pass
+
+	def run( self, iterations ):
+		for i in xrange( iterations ):
+			self.__setup()
+			self.__chooseDoor()
+			self.__revealDoor()
+			self.__chooseDoor()
+			self.__revealDoor()
+
+	def __str__( self ):
+		out = []
+		return "Actions: %s" % (self.actions)
+		#return "\n".join(map(str,self.actions))
+
+
+if __name__ == "__main__":
+	mh = MontyHall( True )
+	#mh.run(10000000)
+	mh.run(1)
+	print( mh )
--- tree_height/opussf/tree.py
+++ tree_height/opussf/tree.py
@@ -1,56 +1,21 @@
 #!/usr/bin/env python
 
 def solution( C, H ):
-	z = len(C) / 2
-	print z
-	out = [ " "*(H-i+z)+((" "*(z+1)).join([ C for j in range(i+1) ]) ) for i in range(H) ]
+	out = [ " "*(H-i-1)+(" ".join([ C for j in range(i+1) ]) ) for i in range(H) ]
 	return "\n".join( out )
 
 if __name__=="__main__":
 	import unittest
 
 	print( solution( "***", 10 ))
-	print( solution( "****", 4 ))
-	print( solution( "*", 2 ))
 
 	class testTree( unittest.TestCase ):
 		def test01( self ):
 			out = solution( "*", 2 )
-			self.assertEquals( "  *\n * *", out )
+			self.assertEquals( " *\n* *", out )
 		def test02( self ):
 			out = solution( "t", 5 )
 			self.assertEquals( "    t\n   t t\n  t t t\n t t t t\nt t t t t", out )
-		def test03( self ):
-			out = solution( "123", 3 )
-			self.assertEquals( )
 
 	unittest.main()
-
-"""
-   12           -- 2
- 12  12         -- 1
-12 12 12        -- 0
-
-    123         -- 4 spaces
-  123 123       -- 2
-123 123 123
-
-
-         1234            -- 9 spaces    i = 0
-      1234  1234         -- 6           i = 1
-   1234  1234  1234      -- 3           i = 2
-1234  1234  1234  1234   -- 0           i = 3
-
-(len(C) - 1) * i     = ( 4 - 1 ) * 3      3 = H - 1
-
-9 = 3 * 3  -   ( 3 * i )
-
-
-
-H
-
-3
-
-"""
-
 
