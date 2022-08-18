cc7e68a0228014bfa6a6019e5beeaac34cc10d22
opussf <github@zz9-za.com>
2022-08-18 01:20:58 -0700

    Meh.   Try this with pointers

---
+++ burrows_wheeler_transform/README.md
@@ -0,0 +1,4 @@
+Author the Burrows Wheeler Transform:
+
+https://en.wikipedia.org/wiki/Burrows%E2%80%93Wheeler_transform
+
---
+++ burrows_wheeler_transform/opussf/bwt.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+
+def BETEncode( strIn ):
+	"""Takes a string, returns a string"""
+	if not "\002" in strIn and not "\003" in strIn:
+		s = "\002" + strIn + "\003"
+		table = sorted(s[i:]+s[:i] for i in range(len(s)))
+		last_column = [row[-1:] for row in table]
+		return "".join(last_column)
+	else:
+		return none
+
+def BETDecode( strIn ):
+	table = [""] * len(strIn)
+	for i in range(len(strIn)):
+		table = sorted(strIn[i] + table[i] for i in range(len(strIn)))
+		#print( table )
+	strOut = [row for row in table if row.endswith("\003")][0]
+	return strOut.rstrip("\003").strip("\002")
+
+print( BETEncode( "BANANA" ) )
+print( BETEncode( "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES" ) )
+print( BETEncode( open(sys.argv[0],"r").read() ) )
+
+print( BETDecode( BETEncode( open(sys.argv[0],"r").read() ) ) )
+#print( BETDecode( "EXEREDSEEND.HREENRRREVHS.TETTT.SEO.FUEHHHI......OEI" ) )
