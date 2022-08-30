4450ac099ac40758d364e771a27ab8b185c4bd0c
opussf <github@zz9-za.com>
2022-08-30 00:45:47 -0700

    Yaya

--- burrows_wheeler_transform/opussf/bwt.py
+++ burrows_wheeler_transform/opussf/bwt.py
@@ -3,8 +3,8 @@ import sys
 
 def BETEncode( strIn ):
 	"""Takes a string, returns a string"""
-	if not "\002" in strIn and not "\003" in strIn:
-		s = "\002" + strIn + "\003"
+	if not "\003" in strIn:
+		s = strIn + "\003"
 		table = sorted(s[i:]+s[:i] for i in range(len(s)))
 		last_column = [row[-1:] for row in table]
 		return "".join(last_column)
@@ -17,9 +17,11 @@ def BETDecode( strIn ):
 		table = sorted(strIn[i] + table[i] for i in range(len(strIn)))
 		#print( table )
 	strOut = [row for row in table if row.endswith("\003")][0]
-	return strOut.rstrip("\003").strip("\002")
+	return strOut.rstrip("\003")
 
 print( BETEncode( "BANANA" ) )
+print( BETEncode( "Altanzaya" ) )
+print( BETEncode( "Charles" ) )
 print( BETEncode( "SIX.MIXED.PIXIES.SIFT.SIXTY.PIXIE.DUST.BOXES" ) )
 print( BETEncode( open(sys.argv[0],"r").read() ) )
 
