61f3f2e77950312813b4849ea72ea7d71c1f57c5
opussf <github@zz9-za.com>
2023-05-24 00:37:12 -0700

    New

--- Fractran/opussf/fractran.lua
+++ Fractran/opussf/fractran.lua
@@ -1,8 +1,27 @@
 #!/usr/bin/env lua
 
+
+
+function factor( inVal, p )
+	outVals = {}
+	for f = 2, inVal / 2 do
+		while (inVal / f) % 1 == 0 do
+			outVals[f] = outVals[f] and outVals[f] + 1 or 1
+			inVal = inVal / f
+		end
+	end
+	if p then
+		for i,v in pairs(outVals) do
+			print(i.."^"..v)
+		end
+	end
+	return outVals
+end
+
 prog = { 455, 33, 11, 13, 1, 11, 3, 7, 11, 2, 1 ,3 }
 
 input = 72
+factor(input, true)
 
 maxInstructon = #(prog)/2
 
@@ -11,9 +30,10 @@ n = input
 inst = 1
 
 while inst <= maxInstructon do
-	result = n * prog[(inst*2)-1] / prog[(inst*2)]
-	print( n.."*"..prog[inst*2-1].."/"..prog[inst*2].."="..result )
+	numerator, denominator = prog[inst*2-1], prog[inst*2]
+	result = n * numerator / denominator
 	if result % 1 == 0 then
+		print( n.." * "..numerator.."/"..denominator.." = "..result )
 		n = result
 		inst = 1
 	else
@@ -22,3 +42,6 @@ while inst <= maxInstructon do
 end
 
 print(n)
+
+factor( n,true )
+
