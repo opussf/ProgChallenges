1d4cb2157957ac0272cb8b65fafa21cde7222592
opussf <github@zz9-za.com>
2023-05-27 15:44:21 -0700

    Rewrite to make it work with factors and powers.

--- Fractran/opussf/fractran.lua
+++ Fractran/opussf/fractran.lua
@@ -1,8 +1,9 @@
 #!/usr/bin/env lua
 
 function factor( inVal, p )
+	if p then print(inVal..">") end
 	outVals = {}
-	for f = 2, inVal / 2 do
+	for f = 2, inVal do
 		while (inVal / f) % 1 == 0 do
 			outVals[f] = outVals[f] and outVals[f] + 1 or 1
 			inVal = inVal / f
@@ -12,38 +13,64 @@ function factor( inVal, p )
 		end
 	end
 	if p then
-		for i,v in pairs(outVals) do
-			print(i.."^"..v)
-		end
+		printFactors( outVals )
 	end
 	return outVals
+end
+
+function printFactors( t )
+	for f,p in pairs( t ) do
+		io.write(f.."^"..p.." * ")
+	end
+	print()
 end
 
 nums = {455, 11,  1, 3, 11, 1}
 dens = { 33, 13, 11, 7,  2, 3}
 
-input = (2^5)*(3^3)
+input = (2^5)*(3^5)
 
 factor(input, true)
 
 maxInstructon = #(nums)
 
+for i,_ in ipairs( nums ) do
+	nums[i] = factor(nums[i])
+	dens[i] = factor(dens[i])
+end
+
 n = input
+n = factor( n )
 
 inst = 1
-
 while inst <= maxInstructon do
 	numerator, denominator = nums[inst], dens[inst]
-	result = n * numerator / denominator
-	if result % 1 == 0 then
-		print( n.." * "..numerator.."/"..denominator.." = "..result )
-		n = result
+	useThis = true
+	for f, p in pairs( denominator ) do
+		if not n[f] or n[f]<p then
+			useThis = false
+		end
+	end
+	if useThis then
+		-- Subtract
+		for f,p in pairs( denominator ) do
+			n[f] = n[f] - p
+			if n[f] == 0 then n[f] = nil end
+		end
+		-- Add
+		for f,p in pairs( numerator ) do
+			n[f] = n[f] and n[f] + p or p
+		end
 		inst = 1
 	else
 		inst = inst + 1
 	end
 end
 
-print(n)
+r = 1
+for i,p in pairs( n ) do
+	print( i.."^"..p )
+	r = r * ( i ^ p )
+end
 
-factor( n,true )
+print( r )
