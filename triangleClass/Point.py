28cb1d4402c8e66e0e6d231e05ba5479833258b7
opussf <github@zz9-za.com>
2023-07-21 01:34:06 -0700

    Flushing out a Point

--- triangleClass/Point.py
+++ triangleClass/Point.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python3
+
 class Point( object ):
-    pass
+    def __init__( self, x, y ):
+        self.__x = x
+        self.__y = y
+    @property
+    def x( self ):
+        return self.__x
+    @property
+    def y( self ):
+        return self.__y
+    def complex( self ):
+        return complex( self.__x, self.__y )
 
 if __name__ == "__main__":
     import unittest
     class TestPoint( unittest.TestCase ):
         def setUp( self ):
-            self.P = Point()
+            self.P = Point( 5, 6 )
         def tearDown( self ):
             self.P = None
-        def testPoint01( self ):
-            pass
+        def test_Point_getX( self ):
+            self.assertEqual( 5, self.P.x )
+        def test_Point_getY( self ):
+            self.assertEqual( 6, self.P.y )
+        def test_Point_cannot_assign_x( self ):
+            with self.assertRaises( AttributeError ):
+                self.P.x = 10
+            self.assertEqual( 5, self.P.x )
+        def test_Point_cannot_assign_y( self ):
+            with self.assertRaises( AttributeError ):
+                self.P.y = 10
+            self.assertEqual( 6, self.P.y )
+        def test_Point_complex( self ):
+            self.assertEqual( 5+6j, self.P.complex() )
 
     unittest.main()
