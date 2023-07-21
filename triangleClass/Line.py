5f8c4a7b76cf997321a889f934a45dd468cf3e7c
opussf <github@zz9-za.com>
2023-07-21 01:45:57 -0700

    Refine Line

--- triangleClass/Line.py
+++ triangleClass/Line.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 ### Line
 import Point
+import math
 
 class Line( object ):
     def __init__( self, point1, point2 ):
-        pass
+        self.point1 = point1
+        self.point2 = point2
+    @property
+    def length( self ):
+        return math.sqrt( math.pow( self.point2.x - self.point1.x, 2) + math.pow( self.point2.y - self.point1.y, 2))
 
 if __name__ == "__main__":
     import unittest
     class TestLine( unittest.TestCase ):
         def setUp( self ):
-            self.L = Line( Point.Point( 0, 0 ), Point.Point( 1, 1, ) )
+            self.L = Line( Point.Point( 0, 0 ), Point.Point( 1, 1 ) )
         def tearDown( self ):
             self.L = None
-        def testLine01( self ):
-            pass
+        def test_Line_getLength( self ):
+            self.assertAlmostEqual( 1.4142135623730951, self.L.length )
 
     unittest.main()
